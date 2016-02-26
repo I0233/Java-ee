b4f3ef22d82fd26bfa5f8f2e98b2c5c4d4a04402
Ali Nadhum <i0233@student.jamk.fi>
2016-02-26 23:04:20 +0200

    Virhekäsittely koodi muutettu

--- JSF_Project/src/Jsf_Fishes/FishController.java
+++ JSF_Project/src/Jsf_Fishes/FishController.java
@@ -1,5 +1,6 @@
 package Jsf_Fishes;
 
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.*;
 import javax.faces.context.FacesContext;
 
@@ -14,16 +15,31 @@ public class FishController implements java.io.Serializable  {
 	
 	/* @return null (error) tai success */
     public String tarkista() {
-        FacesContext facesContext = FacesContext.getCurrentInstance();        
+        FacesContext facesContext = FacesContext.getCurrentInstance();       
+        
+        //Keeping faces message after redirect (JSF)
+        facesContext.getExternalContext().getFlash().setKeepMessages(true);
+        
         // JSF:ssa luodun beanin nimell‰ p‰‰st‰‰n olioon kiinni "fish" (faces-config.xml)
         Fish f = (Fish)facesContext.getExternalContext().getSessionMap().get("fish");
         int a = f.getLength();
         int b = f.getWeight();
         
-        if (a <= 0 || b <= 0) { // Onko pituus tai paino nolla tai negatiivinen!
+        if (a <= 0 && b <= 0) { // Onko pituus tai paino nolla tai negatiivinen!
+        	facesContext.addMessage(null, new FacesMessage("Pituus ja paino arvot on muutettava enemm‰n kun nolla!"));
             return "FishError"; // virhe. ohjaus virhesivulle - FishError.xhtml
         }
-        return "FishOutput"; // Suora ohjaus tiedostoon - FishOutput.xhtml
+        else if (b <= 0){
+        	facesContext.addMessage(null, new FacesMessage("Painon on oltava enemm‰n kun nolla!"));
+            return "FishError"; 
+        }
+        else if (a <= 0){
+        	facesContext.addMessage(null, new FacesMessage("Pituuden on oltava enemm‰n kun nolla!"));
+            return "FishError"; 
+        }
+        else{
+        	return "FishOutput"; // Suora ohjaus tiedostoon - FishOutput.xhtml
+        }
     }
 
 }
